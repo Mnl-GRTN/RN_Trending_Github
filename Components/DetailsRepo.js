01589e1809f86419d347601cc811c80d62c4c288
Mnl-GRTN <anotix13@gmail.com>
2023-12-04 20:05:37 +0100

    TEST

--- Components/DetailsRepo.js
+++ Components/DetailsRepo.js
@@ -20,7 +20,7 @@ export const DetailsRepo = ({route, navigation}) => {
 
     const getRepoInfoFromApiAsync = async () => {
         try {
-            const token = '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';
+            const token = 'YOUR_TOKEN_HERE';
             const requestOptions = {
                 headers: {
                     Authorization: `Bearer ${token}`,
